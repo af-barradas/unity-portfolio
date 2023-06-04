518eb7be45caf1ace8a0c16aa4179541d7461d3f
unknown <afbarradas@gmail.com>
2023-06-04 17:17:46 +0100

    new serializable data class

---
+++ Assets/v2.2/Scripts/Data.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Data
+{
+    public List<Expense> expenses;
+
+    public Data()
+    {
+        // Initialize list
+        this.expenses = new List<Expense>();
+    }
+
+    public Data(Data data)
+    {
+        // Initialize list
+        this.expenses = data.expenses;
+    }
+}
