6ad6eea9ddb2e4490e2d9459dd577c64b2b37af3
unknown <afbarradas@gmail.com>
2023-07-05 19:02:21 +0100

    deleted v2.1

--- Assets/v2.1/Scripts/IData_Manager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IData_Manager
-{
-    void LoadData(Data data);
-    void SaveData(ref Data data);
-}
